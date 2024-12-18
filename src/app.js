512d1c7c60c049f52cc9cbeca53fb01470d2eaf9
yash shah <139542346+Yashshah-official@users.noreply.github.com>
2024-12-18 11:41:16 +0530

    First commit

---
+++ src/app.js
@@ -0,0 +1,53 @@
+import  express from 'express';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import dotenv from "dotenv";
+import methodOverride from 'method-override'
+import ejsMate from 'ejs-mate'
+
+
+dotenv.config({
+    path: './.env'
+  })
+
+
+
+const app = express();
+const port = 5000;
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, "../public")))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.use(methodOverride('_method'));
+
+app.use(express.static(path.join(__dirname, "/public")))
+
+
+app.get('/', (req,res) => {
+    res.render('index.ejs');
+  })
+
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+
+// connectDB()
+// .then(() => {
+//   try {
+//     app.listen(port, () => {
+//       console.log(`Server running at http://localhost:${port}`);
+//     });
+//   } catch (error) {
+//       console.log("Error : ", error);
+//   }
+// })
