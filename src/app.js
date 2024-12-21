83bc360b392603a32604b2c8f72c58195b34fca8
Yashshah-official <yashshah28072004@gmail.com>
2024-12-21 11:51:26 +0530

    Added Model of Crop Prediction

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import { fileURLToPath } from 'url';
 import dotenv from "dotenv";
 import methodOverride from 'method-override'
 import ejsMate from 'ejs-mate'
+import { spawn } from 'child_process';
+
 
 
 dotenv.config({
@@ -35,11 +37,39 @@ app.use(express.static(path.join(__dirname, "/public")))
 
 app.get('/', (req,res) => {
     res.render('index.ejs');
-  })
+  });
+
+app.get('/crop_prediction',(req,res)=>{
+  res.render('cropprediction.ejs');
+});
+
+app.post('/predict',(req,res)=>{
+  let {N,P,K,temperature,humidity,ph,rainfall} = req.body;
+  // console.log(N,P,K,temperature,humidity,ph,rainfall)
+  const obj = {N: N,P: P, K: K ,temperature: temperature,humidity: humidity, ph: ph,rainfall: rainfall}
+  const pythonscript = "D:/Full stack Projects/Agrivista/src/utils/crop_prediction.py"
+  const childPython = spawn('python',[pythonscript, JSON.stringify(obj)])
+
+childPython.stdout.on('data',(data)=>{
+    console.log(`stdout: ${data}`)
+    res.send(`${data}`)
+});
+
+childPython.stderr.on('data',(data)=>{
+    console.error(`stderr: ${data}`)
+});
+
+childPython.on('close',(code)=>{
+    console.log(`Child process exited on code: ${code}`)
+});
+
+})
 
   app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
   });
+
+  
 
 // connectDB()
 // .then(() => {
